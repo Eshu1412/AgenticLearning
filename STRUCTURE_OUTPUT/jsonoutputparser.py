f451cf411ec4b1bee6555150088e0bcb56312e95
Tushar Maurya <mauryatushar115@gmail.com>
2026-06-24 15:18:34 +0530

    file added JsonParser

---
+++ STRUCTURE_OUTPUT/jsonoutputparser.py
@@ -0,0 +1,22 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+load_dotenv()
+
+llm=HuggingFaceEndpoint(
+    repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
+    task='text-generation',
+    temperature=1.5
+)
+chat_model=ChatHuggingFace(llm=llm)
+parser=JsonOutputParser()
+template1=PromptTemplate(
+    template="Give me the name,profession,city and age of fictional character \n{format_instruction}",
+    input_variables=[],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain=template1 | chat_model | parser
+result=chain.invoke(dict())
+print(result)
